1f290b96f9637752da102c14d17940336a56b94f
root <110@qq.com>
2023-06-01 14:26:40 +0800

    Initial commit

---
+++ src/main/java/com/xianglongcarshop/XiangLongCheHangApplication.java
@@ -0,0 +1,13 @@
+package com.xianglongcarshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiangLongCheHangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiangLongCheHangApplication.class, args);
+    }
+
+}
